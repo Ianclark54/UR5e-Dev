a113f3422b4591c8a9d001a31192144dc921f765
Ianclark54 <ianclark54@github.com>
2026-04-03 12:09:12 -0500

    Add ur5e_ws OLD WORKING folder with all workspace snapshots

---
+++ ur5e_ws OLD WORKING/ur5e_ws_ian/build/ur_cell_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/iac933-admin/ur5e_ws_ian/src/UR5e_3DP/src/ur_cell_config
+# Build directory: /home/iac933-admin/ur5e_ws_ian/build/ur_cell_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ ur5e_ws OLD WORKING/ur5e_ws_ian_testing/build/ur_cell_config/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/iac933-admin/ur5e_ws_ian_testing/install/ur5e_description/share/ur5e_description/package.sh"
